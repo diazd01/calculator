a41b02104df15d3430703b06f297dd03792a86bf
diazd01 <48544120+diazd01@users.noreply.github.com>
2020-05-28 03:21:53 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,18 +4,21 @@ let operators = document.querySelectorAll('.operators');
 const numbers = document.querySelectorAll('.numbers');
 const equals = document.querySelector('.equals');
 const buttons = document.querySelectorAll('.btn');
+const clearButton = document.querySelector('.clear');
 //Variables:
 entry.innerText = 0;
+let decimalCount = 0;
 let arr = [];
 let firstNum;
 let secondNum;
 let createdNum;
 let operator;
 let answer;
+let equalsClicked = false;
 
 const currentOperator = {
     op: '',
-    active: false
+    active: false,
 };
 
 //Functions:
@@ -39,16 +42,44 @@ const createNumber = function (num) {
 
     if (arr.length < 9) {
         arr.push(num);
-        if (arr[0] === '0') {
-            arr.splice(0, 1);
-            return;
-        }
+    }
+    if (num === '.') {
+        ++decimalCount;
+    }
+    if (decimalCount > 1 && num === '.') {
+        arr.pop();
+    }
+    if (arr[0] === '.') {
+        arr.unshift(0);
     }
     num = arr.join('');
     entry.innerText = num;
     return num;
 };
 
+const defineNumbers = function () {
+    if (firstNum === undefined) {
+        firstNum = createdNum;
+        createdNum = undefined;
+    } else if (secondNum === undefined) {
+        secondNum = createdNum;
+        createdNum = undefined;
+    }
+}
+
+const clearCalculator = function () {
+    entry.innerText = 0;
+    decimalCount = 0;
+    arr = [];
+    firstNum = undefined;
+    secondNum = undefined;
+    createdNum = undefined;
+    operator = undefined;
+    answer = undefined;
+    equalsClicked = false;
+    currentOperator.active = false;
+    document.body.style.backgroundColor = 'white';
+}
 
 //Events:
 numbers.forEach((number) => {
@@ -61,7 +92,9 @@ operators.forEach((op) => {
     op.addEventListener('click', (e) => {
 
         arr = [];
+        decimalCount = 0;
         currentOperator.active = true;
+
         switch (e.target.innerText) {
 
             case '÷':
@@ -79,20 +112,16 @@ operators.forEach((op) => {
             default:
                 return;
         }
-
-        if (firstNum === undefined) {
-            firstNum = createdNum;
-            createdNum = undefined;
-        } else if (secondNum === undefined) {
-            secondNum = createdNum;
-            createdNum = undefined;
+        if (equalsClicked) {
+            secondNum = undefined;
         }
+
+        defineNumbers();
 
         if (firstNum !== undefined && secondNum !== undefined) {
             operate(firstNum, secondNum);
             secondNum = undefined;
             firstNum = answer;
-            console.log(answer);
         }
         currentOperator.op = operator;
         console.log(`${firstNum} ${secondNum}`);
@@ -102,22 +131,29 @@ operators.forEach((op) => {
 
 /* EQUALS SIGN EVENT */
 equals.addEventListener('click', (e) => {
-    console.log('ye'); 
-    if (firstNum === undefined) {
-        firstNum = createdNum;
-        createdNum = undefined;
-    } else if (secondNum === undefined) {
-        secondNum = createdNum;
-        createdNum = undefined;
-    }
+    equalsClicked = true;
+    defineNumbers();
 
     if (firstNum !== undefined && secondNum !== undefined) {
         operate(firstNum, secondNum);
         firstNum = answer;
-        console.log(answer);
-    }
-    if (firstNum !== undefined && secondNum === undefined && currentOperator.active) {
+    } else if (firstNum !== undefined && secondNum === undefined && currentOperator.active) {
         secondNum = firstNum;
         operate(firstNum, secondNum);
+        firstNum = answer;
     }
+    console.log(`${firstNum} ${secondNum}`);
+    console.log(answer);
+});
+
+/* CLEAR BUTTON EVENT: */
+
+clearButton.addEventListener('click', (e) => {
+    clearCalculator();
+});
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        document.body.style.backgroundColor = '#90EE90';
+    });
 });
